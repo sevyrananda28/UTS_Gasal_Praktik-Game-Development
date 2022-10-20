00708d3ec8d2181dd67e27f1d09d473d8c13624a
sevyrananda28 <100419406+sevyrananda28@users.noreply.github.com>
2022-10-20 17:42:01 +0700

    Add files via upload

---
+++ ProjectGameDevelopment/scenes/Dino.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+
+var gravity = 50
+var velocity =Vector2.ZERO
+var jump_force = 600
+
+func _physics_process(delta):
+	velocity.y += gravity
+	move_and_slide(velocity, Vector2.UP)
+	
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = -jump_force
